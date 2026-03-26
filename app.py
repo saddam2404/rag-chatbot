544633abd6082b8e12396a4b936083bb032363da
Mohammed Saddam <saddam24tech@gmail.com>
2026-03-26 20:51:42 +0530

    Added embeddings and FAISS vector store

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 import streamlit as st
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+load_dotenv()
 
 st.title("RAG Chatbot")
 
@@ -23,6 +28,12 @@ chunks = text_splitter.split_documents(documents)
 
 st.write("Total chunks created:", len(chunks))
 
+embeddings = OpenAIEmbeddings()
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+st.success("FAISS vector store created successfully!")
+
 if chunks:
     st.subheader("First chunk preview")
-    st.write(chunks[0].page_content)
+    st.write(chunks[0].page_content[:500])
